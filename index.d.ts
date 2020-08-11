0e73cb638a2e7b68a5a2915d7787a76e9597362d
Murillo Nicacio de Maraes <mnmaraes@gmail.com>
2020-08-11 19:41:30 +0100

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -2,7 +2,7 @@ declare module "react-native-geocoding" {
   export function init(apiKey: string, options?: Object): void;
   export function isInit(): boolean;
   export function setApiKey(API_KEY: string): void;
-  export function from(...params: any[]): Promise<void>;
+  export function from(...params: any[]): Promise<any>;
   export function getFromLocation(address: string): Promise<any>;
   export function getFromLatLng(lat: number, lng: number): Promise<any>;
 }
